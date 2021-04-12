9d0b5de0664f34539bb40ab233aea44182474198
kush0511 <kush.g2100@gmail.com>
2021-04-12 11:45:29 +0800

    rw

---
+++ server/models/allComments.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reply = new Schema({
+    studentID:{
+        type: String,
+        required: true
+    },
+    replyID: {
+        type: Number,
+        required: true
+    },
+    replyBody: {
+        type:String,
+        require:true
+    }
+});
+
+//definining the schema of a comment
+const comments = new Schema({
+    studentID:{
+        type: String,
+        required: true
+    },
+    commentID: {
+        type: Number,
+        required: true
+    },
+    commentBody: {
+        type: String,
+        required: true
+    },
+    replies:[reply]
+});
+
+const allComments = new Schema({
+    refID: {
+        type: Number,
+        default: 1
+    },
+    comments: [comment],
+    default: []
+})
+
+//making the mongoose model and exporting it
+const Comments= mongoose.model('comments', comments, 'Comments');
+module.exports= Comments;
