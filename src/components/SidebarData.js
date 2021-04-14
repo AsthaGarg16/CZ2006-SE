ec970d135a807bc45faba09c031411f774824025
liew312 <56454775+liew312@users.noreply.github.com>
2021-04-14 17:11:04 +0800

    Changed sidebar icons

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
-import * as FcIcons from 'react-icons/fc';
-import * as BiIcons from 'react-icons/bi';
-import * as GoIcons from 'react-icons/go';
+import React from "react";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as FcIcons from "react-icons/fc";
+import * as BiIcons from "react-icons/bi";
+import * as GoIcons from "react-icons/go";
+import EventIcon from "@material-ui/icons/Event";
+import HomeIcon from "@material-ui/icons/Home";
+import FindInPageIcon from "@material-ui/icons/FindInPage";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
 
 export const SidebarData = [
   {
-    title: 'Home',
-    path: '/',
-    icon: <AiIcons.AiFillHome />,
-    cName: 'nav-text'
+    title: "Home",
+    path: "/",
+    icon: <HomeIcon style={{ margin: 10 }} />,
+    cName: "nav-text",
   },
   {
-    title: 'Plan Timetable',
-    path: '/planner',
-    icon: <FcIcons.FcPlanner />,
-    cName: 'nav-text'
+    title: "Plan Timetable",
+    path: "/planner",
+    icon: <EventIcon style={{ margin: 10 }} />,
+    cName: "nav-text",
   },
   // {
   //   title: 'Share Time Table',
@@ -25,15 +29,15 @@ export const SidebarData = [
   //   cName: 'nav-text'
   // },
   {
-    title: 'Find Common Free Time Slots',
-    path: '/findcommon',
-    icon: <BiIcons.BiFileFind />,
-    cName: 'nav-text'
+    title: "Find Common Free Time Slots",
+    path: "/findcommon",
+    icon: <FindInPageIcon style={{ margin: 10 }} />,
+    cName: "nav-text",
   },
   {
-    title: 'Discussion Forum',
-    path: '/discuss',
-    icon: <GoIcons.GoCommentDiscussion />,
-    cName: 'nav-text'
-  }
+    title: "Discussion Forum",
+    path: "/discuss",
+    icon: <QuestionAnswerIcon style={{ margin: 10 }} />,
+    cName: "nav-text",
+  },
 ];
