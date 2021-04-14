91a2aa26a4d6ae78808b97300446b63e43bc5174
liew312 <56454775+liew312@users.noreply.github.com>
2021-04-14 17:10:53 +0800

    Changed color position

--- src/components/resources.js
+++ src/components/resources.js
@@ -1,51 +1,41 @@
 export const resourcesData = [
   {
-    text: "Course1",
     id: 1,
-    color: "#7C0A02",
-  },
-  {
-    text: "Course2",
-    id: 2,
     color: "#E84A5F",
   },
   {
-    text: "Course3",
-    id: 3,
+    id: 2,
     color: "#FF847C",
   },
   {
-    text: "Course4",
-    id: 4,
+    id: 3,
     color: "#FECEA8",
   },
   {
-    text: "Course5",
-    id: 5,
+    id: 4,
     color: "#99B898",
   },
   {
-    text: "Course6",
-    id: 6,
+    id: 5,
     color: "#DCD0FF",
   },
   {
-    text: "Course7",
-    id: 7,
+    id: 6,
     color: "#8C77BE",
   },
   {
-    text: "Course8",
+    id: 7,
+    color: "#7C0A02",
+  },
+  {
     id: 8,
     color: "#581845",
   },
   {
-    text: "Course9",
     id: 9,
     color: "#122C34",
-  }
+  },
 ];
-
 
 // export const resourcesData = [
 //   {
